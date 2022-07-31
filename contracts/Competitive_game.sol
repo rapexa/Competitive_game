da12e874b87b5b798083b2dc83d23e659fdead0c
ali <aliali@gmail.com>
2022-07-30 17:53:13 -0700

    Start game function compelete

--- contracts/Competitive_game.sol
+++ contracts/Competitive_game.sol
@@ -10,6 +10,18 @@ contract Game is ReentrancyGuard{
     event StartGameEvent(address indexed user, uint game, uint status);
     event EndGameEvent(address indexed user, uint game, uint status);
 
-    
+function startGame(uint _gameId)  public payable nonReentrant {
+        require((gameUserCount[_gameId] == 0)  || (gameUserCount[_gameId] == 1 && gameToBet[_gameId] == msg.value));
+        require((gameUserCount[_gameId] == 0)  || (gameToUser[_gameId][0] != msg.sender));
+
+        gameStatus[_gameId] = gameUserCount[_gameId] + 1;
+
+        gameToBet[_gameId] = msg.value;
+        gameToUser[_gameId][gameUserCount[_gameId]] = msg.sender;
+        gameUserCount[_gameId]++;
+
+        emit StartGameEvent(msg.sender, _gameId, gameStatus[_gameId]);
+    }
+
 
 }
