fefc655d2c3bb29a07f643e08f84d4947b3d2b61
ali <aliali@gmail.com>
2022-07-30 17:51:33 -0700

    Basics of contract

--- contracts/Competitive_game.sol
+++ contracts/Competitive_game.sol
@@ -1,7 +1,15 @@
 pragma solidity ^0.8.15;
-//import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Game {
+contract Game is ReentrancyGuard{
+    mapping (uint => address[2]) gameToUser;
+    mapping (uint => uint) gameToBet;
+    mapping (uint => uint) gameUserCount;
+    mapping (uint => uint) gameStatus;// gameNotExist=0, gameStart=1, gameRuning=2, gameEnd=3;
 
+    event StartGameEvent(address indexed user, uint game, uint status);
+    event EndGameEvent(address indexed user, uint game, uint status);
+
+    
 
 }
